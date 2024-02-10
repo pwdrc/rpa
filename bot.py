d2fdee6a4a7a2640c6b0bcc075a0b7f97c6262b8
pwdrc <98365041+pwdrc@users.noreply.github.com>
2024-02-10 11:11:27 -0300

    Update bot.py - finalizado, mas dá para melhorar

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import time
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,6 +12,7 @@ driver = webdriver.Chrome()
 base_url = "https://rpachallengeocr.azurewebsites.net/"
 start_button_id = "start" 
 table_id = "tableSandbox"
+extracted_data_file = "invoices.csv"
 
 # Inicia a navegação
 driver.get(base_url)
@@ -18,13 +20,13 @@ start_button = driver.find_element(By.ID, start_button_id)
 start_button.click()
 
 # Aguarda o carregamento da tabela
-time.sleep(10)
+time.sleep(3)
 
 # Extrai dados da tabela
 table_data = []
 next = 3
 while(next > 0):
-    time.sleep(1)
+    #time.sleep(1)
     rows = driver.find_elements(By.XPATH, f"//table[@id='{table_id}']/tbody/tr")
 
     for row in rows:
@@ -34,11 +36,11 @@ while(next > 0):
         # Adiciona dados à tabela apenas se o texto da célula não estiver vazio
         if cell_text:
             cells = row.find_elements(By.TAG_NAME, "td")
-            table_data.append({
-                "ID": cells[1].text,
-                "Due Date": cells[2].text,
-                "Invoice URL": cells[3].find_element(By.TAG_NAME, "a").get_attribute("href")
-            })
+            table_data.append([
+                cells[1].text,
+                cells[2].text,
+                cells[3].find_element(By.TAG_NAME, "a").get_attribute("href")
+            ])
     next_element = driver.find_element(By.CLASS_NAME, "next")
     next_element.click()
     next = next - 1
@@ -46,6 +48,19 @@ while(next > 0):
 # Imprime os dados da tabela
 print(table_data)
 
+# Escreve os dados no arquivo invoices.csv
+with open(extracted_data_file, 'w', newline='') as file_csv:
+    writer_csv = csv.writer(file_csv)
+
+    writer_csv.writerow(["ID", "Due Date", "Invoice URL"])
+    for line in table_data:
+        writer_csv.writerow(line)
+
+print(f'Arquivo "{extracted_data_file}" criado com sucesso.')
+
+# Submete o invoices.csv
+file_input = driver.find_element(By.NAME, "csv")
+file_input.send_keys(os.path.abspath(extracted_data_file))
 
 # Aguarda X segundos antes de fechar
 time.sleep(10)
